17ef2e6a8513f80805f40931cf9293c68bb5ac9d
aidan-starke <starke.aidan@gmail.com>
2021-09-29 16:27:33 +1300

    transfer all cbj tokens to House

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,12 +3,15 @@ const CBJ = artifacts.require('CBJ')
 const Bag = artifacts.require('Bag')
 const House = artifacts.require('House')
 
-module.exports = async function (deployer) {
+module.exports = async function (deployer, _network, accounts) {
     await deployer.deploy(CBJ, 1000000)
     const cbj = await CBJ.deployed()
 
     await deployer.deploy(House, cbj.address)
     const house = await House.deployed()
+
+    await cbj.approve(accounts[0], web3.utils.toWei('1000000'))
+    await cbj.transferFrom(accounts[0], house.address, web3.utils.toWei('1000000'))
 
     await deployer.deploy(Bag, house.address)
     const bag = await Bag.deployed()
