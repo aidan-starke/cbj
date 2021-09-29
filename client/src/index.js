43202792adec575fbb4cd64c661af6c86a6bdd7f
aidan-starke <starke.aidan@gmail.com>
2021-09-29 16:26:31 +1300

    edit initialEntries, add provider/store

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import App from './App';
 import 'antd/dist/antd.css'
 import { ThemeSwitcherProvider } from 'react-css-theme-switcher'
 import { MemoryRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store'
 
 const themes = {
   dark: `${process.env.PUBLIC_URL}/dark-theme.css`,
@@ -15,10 +17,12 @@ const prevTheme = window.localStorage.getItem('theme')
 ReactDOM.render(
   <React.StrictMode>
     <Router
-      initialEntries={['/', '/create', '/dashboard', '/home', '/assets']}
+      initialEntries={['/', '/account', '/bag', '/play']}
     >
       <ThemeSwitcherProvider themeMap={themes} defaultTheme={prevTheme ? prevTheme : 'light'}>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </ThemeSwitcherProvider>
     </Router>
   </React.StrictMode>,
