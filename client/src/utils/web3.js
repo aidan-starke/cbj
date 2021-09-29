bc69078356886cffaf1ef6e3b3bbb9580b0d31c4
aidan-starke <starke.aidan@gmail.com>
2021-09-29 16:23:58 +1300

    connect to web3

---
+++ client/src/utils/web3.js
@@ -0,0 +1,44 @@
+import Web3 from 'web3'
+import detectEthereumProvider from '@metamask/detect-provider'
+import CBJ from '../contracts/CBJ.json'
+import House from '../contracts/House.json'
+import Bag from '../contracts/Bag.json'
+
+function getWeb3 () {
+    return new Promise(async (resolve, reject) => {
+        const provider = await detectEthereumProvider()
+        if (provider) {
+            await provider.request({ method: 'eth_requestAccounts' })
+            try {
+                const web3 = new Web3(window.ethereum)
+                resolve(web3)
+            } catch (err) {
+                reject(err)
+            }
+        } reject('Install Metamask')
+    })
+}
+
+async function getContracts (web3) {
+    const networkId = await web3.eth.net.getId()
+    const cbjDeployedNetwork = CBJ.networks[networkId]
+    const houseDeployedNetwork = House.networks[networkId]
+    const bagDeployedNetwork = Bag.networks[networkId]
+
+    return Promise.all([
+        new web3.eth.Contract(
+            CBJ.abi,
+            cbjDeployedNetwork && cbjDeployedNetwork.address
+        ),
+        new web3.eth.Contract(
+            House.abi,
+            houseDeployedNetwork && houseDeployedNetwork.address
+        ),
+        new web3.eth.Contract(
+            Bag.abi,
+            bagDeployedNetwork && bagDeployedNetwork.address
+        )
+    ])
+}
+
+export { getWeb3, getContracts }
